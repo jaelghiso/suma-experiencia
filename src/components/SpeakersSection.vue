655c9a7177a63f9ade6133c5348a37be2a15e93c
jaelghiso <jael.ghiso@gmail.com>
2021-02-25 08:40:26 -0300

    adding speakers section

---
+++ src/components/SpeakersSection.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <div class="speakersSection-header">
+            <h2 class="speakersSection-title">Speakers</h2>
+        </div>
+        <div class="speakersSection-body grid grid-cols-3 gap-4">
+            <div class="speaker">
+                <div class="speaker-img--wrapper">
+                <img src="@/assets/images/Anna W.jpg" alt="" class="speaker-img">
+                </div>
+                <h4 class="speaker-name">Anna White</h4>
+                <p class="speaker-role">Head of Capability Building - Digital, Novartis</p>
+            </div>
+            <div class="speaker">
+                <div class="speaker-img--wrapper">
+                <img src="@/assets/images/Regina L.jpg" alt="" class="speaker-img">
+                </div>
+                <h4 class="speaker-name">Regina A. Lena</h4>
+                <p class="speaker-role">Diseñadora de Interacción</p>
+            </div>
+            <div class="speaker">
+                <div class="speaker-img--wrapper">
+                <img src="@/assets/images/Darcy V.jpg" alt="" class="speaker-img">
+                </div>
+                <h4 class="speaker-name">Darcy Vergara Panire</h4>
+                <p class="speaker-role">Head of UX en LATAM Airlines</p>
+            </div>
+        </div>
+        <div class="speakersSection-body grid grid-cols-3 gap-4">
+            <div class="speaker">
+                <div class="speaker-img--wrapper">
+                <img src="@/assets/images/Rachel S.jpg" alt="" class="speaker-img">
+                </div>
+                <h4 class="speaker-name">Rachel Ilan Simpson</h4>
+                <p class="speaker-role">Líder de diseño en Google</p>
+            </div>
+            <div class="speaker">
+                <div class="speaker-img--wrapper">
+                <img src="@/assets/images/Mariel B.jpg" alt="" class="speaker-img">
+                </div>
+                <h4 class="speaker-name">Muriel Backman</h4>
+                <p class="speaker-role">Diseñadora UX y de servicios</p>
+            </div>
+            <div class="speaker">
+                <div class="speaker-img--wrapper">
+                <img src="@/assets/images/Nur K.jpg" alt="" class="speaker-img">
+                </div>
+                <h4 class="speaker-name">Nur Karadeniz</h4>
+                <p class="speaker-role">Fundadora de System Design Practice y Business Mentor en Diseño Sustentable en la Universidad de Cambridge</p>
+            </div>
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <hero />
     <main-features-section />
+    <speakers-section />
   </div>
 </template>
 
@@ -9,12 +10,14 @@
 // @ is an alias to /src
 import Hero from "@/components/Hero.vue";
 import mainFeaturesSection from "@/components/MainFeaturesSection.vue";
+import speakersSection from "@/components/SpeakersSection.vue";
 
 export default {
   name: "Home",
   components: {
     Hero,
     mainFeaturesSection,
+    speakersSection,
   },
 };
 </script>
